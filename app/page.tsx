7539a2ce3f03865d8418bfe13c42eb37d6e2e8ff
Dhairya2505 <dhairyasingla250504@gmail.com>
2025-03-14 01:12:46 +0530

    npm package changes and code change

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,50 @@
 "use client";
 
 import { useState } from "react";
-import { Board } from "node-game-chess";
-import { Coordinate_Type, Piece_Type, response } from 'node-game-chess/dist/types'
 
-interface Respose {
-  canMoveto: {
-    x: Coordinate_Type,
-    y: Coordinate_Type
-  }[],
-  canCut: {
-    x: Coordinate_Type,
-    y: Coordinate_Type
-  }[]
-}
+import { Board } from "npm-chess";
+import { coordinate, response,  } from "npm-chess/dist/types";
 
 export default function Home() {
   const [newBoard, setnewBoard] = useState(new Board())
-  const [board, setBoard] = useState<Array<Array<Piece_Type | null>>>(newBoard.getBoard())
-  const [response, setResponse] = useState<Respose>({
-    canCut: [],
-    canMoveto: []
-  })
   const [selectedSquare, setSelectedSquare] = useState<[number, number] | null>(null)
+  const [response, setResponse] = useState<response | null>(null)
+
   const handleSquareClick = (row: number, col: number) => {
     if (selectedSquare) {
-      if(clicked_on_move_or_cut(row as Coordinate_Type, col as Coordinate_Type)){
-        const res = newBoard.move(board, selectedSquare[0] as Coordinate_Type, selectedSquare[1] as Coordinate_Type, row as Coordinate_Type, col as Coordinate_Type)
+      if(clicked_on_move_or_cut(row as coordinate, col as coordinate)){
+        const res = newBoard.move(newBoard, selectedSquare[0] as coordinate, selectedSquare[1] as coordinate, row as coordinate, col as coordinate)
         if(res){
-          setBoard(res)
+          setnewBoard(res)
         }
       }
       setSelectedSquare(null)
-      setResponse({
-        canCut: [],
-        canMoveto: []
-      })
-    } else if (board[row][col]) {
-      setSelectedSquare([row, col])
-      const response: response | null = newBoard.canMoveTo(board, row, col)
-      if(response){
-        setResponse(response)
+      setResponse(null)
+    } else {
+      const res = newBoard.canMoveTo(newBoard, row, col)
+      if(res){
+        setSelectedSquare([row, col])
+        setResponse(res)
       }
     }
   }
 
-  function clicked_on_move_or_cut(row: Coordinate_Type, col: Coordinate_Type){
-    const isMove = response.canMoveto.some((coords) => coords.x === row && coords.y === col);
-    const isCapture = response.canCut.some((coords) => coords.x === row && coords.y === col);
-    if(isMove){
-      return true
-    }
-    
-    if(isCapture){
-      return true
+  function clicked_on_move_or_cut(row: coordinate, col: coordinate){
+
+    const res1 = response?.canMoveto.some((coords) => row == coords.x && col == coords.y)
+    const res2 = response?.canCut.some((coords) => row == coords.x && col == coords.y)
+
+    if(res1 || res2){
+      return true;
     }
     return false;
+
   }
 
-  function check_coords(row: Coordinate_Type, col: Coordinate_Type, isBlack: boolean){
+  function check_coords(row: coordinate, col: coordinate, isBlack: boolean){
 
-    const isMove = response.canMoveto.some((coords) => coords.x === row && coords.y === col);
-    const isCapture = response.canCut.some((coords) => coords.x === row && coords.y === col);
+    const isMove = response?.canMoveto.some((coords) => coords.x === row && coords.y === col);
+    const isCapture = response?.canCut.some((coords) => coords.x === row && coords.y === col);
 
     if(isMove){
       return "bg-green-500"
@@ -77,7 +60,7 @@ export default function Home() {
   return (
     <div className="flex h-screen aspect-square w-full border border-border rounded-md overflow-hidden">
       <div className="w-1/2 grid grid-cols-8 grid-rows-8">
-        {board.map((row, rowIndex) =>
+        {newBoard.getBoard().map((row, rowIndex) =>
           row.map((piece, colIndex) => {
             const isBlack = (rowIndex + colIndex) % 2 === 1
             const isSelected = selectedSquare && selectedSquare[0] === rowIndex && selectedSquare[1] === colIndex
@@ -87,7 +70,7 @@ export default function Home() {
                 key={`${rowIndex}-${colIndex}`}
                 className={`
                   flex items-center justify-center
-                  ${check_coords(rowIndex as Coordinate_Type, colIndex as Coordinate_Type, isBlack)}
+                  ${check_coords(rowIndex as coordinate, colIndex as coordinate, isBlack)}
                   ${isSelected ? "ring-2 ring-blue-500 ring-inset" : ""}
                   cursor-pointer
                   relative
@@ -121,8 +104,8 @@ export default function Home() {
           }),
         )}
       </div>
-      <div className="">
-        
+      <div className="w-1/2">
+         
       </div>
     </div>
   )
